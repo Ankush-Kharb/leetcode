f5661d90f44ba51d4bc27f19f560dd0ecd207dd3
Ankush Kharb <ankushkharb7428@gmail.com>
2026-08-28 14:53:30 +0000

    Add 0069. Sqrt(x) (cpp)

---
+++ problems/0069-sqrtx/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 1;
+        int high = x;
+        int mid;
+        while(low<=high){
+            mid = (low + (high - low)/2);
+            if ((double)mid*mid == x) return mid;
+            if ((double)mid*mid > x)high = mid-1;
+            else low = mid+1;
+        }return high;
+        
+    }
+};
