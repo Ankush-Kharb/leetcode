72755bb757fa9ac2756041dd59c142a768ae3747
Ankush Kharb <ankushkharb7428@gmail.com>
2026-09-05 07:24:44 +0000

    Add 0520. Detect Capital (cpp)

---
+++ problems/0520-detect-capital/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.length();
+        int count = 0;
+        for (int i = 0;i<n;i++){
+            if(isupper(word[i])) count++;
+        }
+        if(count == 0 || count == n || (count == 1 &&  isupper(word[0])))
+        return true ;
+        else return false;
+        
+    }
+};
