140d74a51d6c367a43ab4e696ebcd76b2dd2fa57
Ankush Kharb <ankushkharb7428@gmail.com>
2026-09-12 07:41:25 +0000

    Add 0008. String to Integer (atoi) (cpp)

---
+++ problems/0008-string-to-integer-atoi/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        long long ans= 0;
+        int n = s.length();
+        int sign = 1;
+        int i = 0;
+        while(i<n && s[i]==' '){
+            i++;
+        }
+        if( i<n && (s[i]=='-' || s[i]=='+')){
+            s[i]=='-'? sign = -1: sign = 1;
+            i++;
+        }
+        while(i<n && isdigit(s[i])){
+            ans = ans*10 + (s[i]-'0');
+            i++;
+        
+        if (sign == 1 && ans > INT_MAX) return INT_MAX;
+        if (sign == -1 && -ans < INT_MIN) return INT_MIN;
+        }
+        return ans*sign;
+        }
+    
+};
