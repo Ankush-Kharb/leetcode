071d3ff3af5048eb7715bb76c6be3ee464534a34
Ankush Kharb <ankushkharb7428@gmail.com>
2026-09-23 08:16:52 +0000

    Add 0055. Jump Game (cpp)

---
+++ problems/0055-jump-game/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0 ;
+        int maxReach = 0;
+        if(n == 1){
+            return true;
+        }
+        while(i<n){
+            if(i > maxReach){
+               return false;
+            }
+            maxReach = max(maxReach ,i+nums[i]);
+            i++;
+        }
+        return true ;
+
+    }
+};
