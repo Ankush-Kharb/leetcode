6f16ce25b3e94a4b4f3b359e6b6673320994b628
Ankush Kharb <ankushkharb7428@gmail.com>
2026-09-25 08:31:48 +0000

    Add 0450. Delete Node in a BST (cpp)

---
+++ problems/0450-delete-node-in-a-bst/solution.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findMax(TreeNode* root) {
+        while (root->right != NULL) {
+            root = root->right;
+        }
+        return root->val;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == NULL) return NULL;
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        }
+        else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        }
+        else {
+            if (root->left == NULL && root->right == NULL) {
+                delete root;
+                return NULL;
+            }
+            else if (root->left == NULL) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            else if (root->right == NULL) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+            else {
+                int maxi = findMax(root->left);
+                root->val = maxi;
+                root->left = deleteNode(root->left, maxi);
+            }
+        }
+        return root;
+    }
+};
